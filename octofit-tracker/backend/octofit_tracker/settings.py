361936e619573aa5705ad84e74d0ab80d8f67e9c
Justyna-27 <justyna.walecka@accenture.com>
2026-03-05 12:23:59 +0000

    endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-0srp(sw(fyp@4m7dkbjp(zpbppxkll1drlr%9u@=+@el*(zyk&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
